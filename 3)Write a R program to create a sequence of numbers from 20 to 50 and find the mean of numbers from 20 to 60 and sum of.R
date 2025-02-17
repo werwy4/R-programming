9db83511aedda596511ce33a09e082cfd0595cf4
werwy4 <swathika1911@gmail.com>
2025-02-17 20:43:40 +0530

    Create 3)Write a R program to create a sequence of numbers from 20 to 50 and find the mean of numbers from 20 to 60 and sum of.R

---
+++ 3)Write a R program to create a sequence of numbers from 20 to 50 and find the mean of numbers from 20 to 60 and sum of.R
@@ -0,0 +1,7 @@
+sequence_20_50 <- 20:50
+cat("Sequence from 20 to 50:\n")
+print(sequence_20_50)
+mean_20_60 <- mean(20:60)
+cat("\nMean of numbers from 20 to 60:", mean_20_60, "\n")
+sum_51_91 <- sum(51:91)
+cat("Sum of numbers from 51 to 91:", sum_51_91, "\n")
